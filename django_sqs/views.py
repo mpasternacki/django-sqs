f83c55eaaa321a2c6820ffafa1a0b00366681aee
Maciej Pasternacki <maciej@pasternacki.net>
2009-10-08 15:39:01 +0200

    Cache status view for five minutes to avoid overload.

--- django_sqs/views.py
+++ django_sqs/views.py
@@ -1,10 +1,14 @@
+import time
+
 from django.http import HttpResponse
+from django.views.decorators.cache import cache_page
 
 import django_sqs
 
+@cache_page(5*60)
 def status(request):
     """Returns a simple plain text rendering of queue status."""
-    response_text = []
+    response_text = ['Queue status as of %s GMT\n' % time.strftime("%F %T", time.gmtime())]
     for label, queue in django_sqs.queues.items():
         q = queue.get_queue()
         response_text.append( "%s\t%s" % (label, q.count()) )
