8c4ab2450f383e89ebfe8f0af104ff496d8c4670
Adrien Lemaire <lemaire.adrien@gmail.com>
2012-06-22 17:16:48 +0900

    version bumped, Pypi update

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 try:
-	from setuptools import setup, find_packages
+    from setuptools import setup, find_packages
 except ImportError:
-	from ez_setup import use_setuptools
-	use_setuptools()
-	from setuptools import setup, find_packages
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
 
 setup(
     name='django-sqs',
-    version="0.1",
+    version="0.2",
     description='django FormFields using the Chosen javascript plugin for jQuery',
     author='Maciej Pasternacki',
     author_email='maciej@pasternacki.net',
@@ -22,7 +22,7 @@ setup(
         'Programming Language :: Python',
         'Framework :: Django',
     ],
-	include_package_data=True,
-	zip_safe=False,
+    include_package_data=True,
+    zip_safe=False,
 )
 
